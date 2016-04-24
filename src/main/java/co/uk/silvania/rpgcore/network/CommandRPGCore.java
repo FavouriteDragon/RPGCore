3b7c4eb054399c0f4b1e334a77f7e14014d0cb45
Flenix <hedgesj@hotmail.co.uk>
2016-04-24 22:49:54 +0100

    Reset Player command (just for bal!) and list skills command

--- src/main/java/co/uk/silvania/rpgcore/network/CommandRPGCore.java
+++ src/main/java/co/uk/silvania/rpgcore/network/CommandRPGCore.java
@@ -2,14 +2,20 @@ package co.uk.silvania.rpgcore.network;
 
 import java.util.ArrayList;
 
+import co.uk.silvania.rpgcore.RPGCore;
 import co.uk.silvania.rpgcore.RPGUtils;
+import co.uk.silvania.rpgcore.RegisterSkill;
+import co.uk.silvania.rpgcore.skills.EquippedSkills;
+import co.uk.silvania.rpgcore.skills.GlobalLevel;
 import co.uk.silvania.rpgcore.skills.SkillLevelBase;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.ICommandSender;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.event.HoverEvent;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.EnumChatFormatting;
+import net.minecraft.util.IChatComponent;
 
 public class CommandRPGCore extends CommandBase {
 
@@ -35,6 +41,8 @@ public class CommandRPGCore extends CommandBase {
 		if (args[0].equalsIgnoreCase("help")) {
 			sender.addChatMessage(new ChatComponentText(red + "/rpgcore help" + green + " - Displays this help message"));
 			sender.addChatMessage(new ChatComponentText(red + "/rpgcore addxp <skillid> <amount> [player]" + green + " - Add amount XP to skill ID, to yourself or player."));
+			sender.addChatMessage(new ChatComponentText(red + "/rpgcore list" + green + " - List all currently loaded skills"));
+			sender.addChatMessage(new ChatComponentText(red + "/rpgcore reset <player>" + green + " - Reset ALL of the specified players skills and XP."));
 		}
 		if (args[0].equalsIgnoreCase("addxp") || args[0].equalsIgnoreCase("xpadd")) {
 			if (args.length == 4) {
@@ -56,6 +64,50 @@ public class CommandRPGCore extends CommandBase {
 			} else {
 				sender.addChatMessage(new ChatComponentText(red + "Invalid arguments! /rpgcore addxp <skillid> <amount> [player]"));
 			}
+		}
+		if (args[0].equalsIgnoreCase("list")) {
+			ArrayList<String> list = new ArrayList<String>();
+			for (int i = 0; i < RegisterSkill.skillList.size(); i++) {
+				SkillLevelBase skill = RegisterSkill.skillList.get(i);
+				list.add(skill.nameFormat() + "[" + skill.shortName() + "] " + skill.skillName() + " (" + skill.skillId + ")\n");
+			}
+
+			IChatComponent chat = new ChatComponentText(green + "Hover here to view skill list.");
+			chat.getChatStyle().setChatHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ChatComponentText(list.toString())));
+			
+			sender.addChatMessage(new ChatComponentText(green + "").appendSibling(chat));
+		}
+		if (args[0].equalsIgnoreCase("reset")) {
+			if (args[1].isEmpty()) {
+				sender.addChatMessage(new ChatComponentText(red + "Please specify a player!"));
+			}
+			ArrayList<EntityPlayer> players = (ArrayList<EntityPlayer>) sender.getEntityWorld().playerEntities;
+			for (int i = 0; i < players.size(); i++) {
+				if (players.get(i).getCommandSenderName().equalsIgnoreCase(args[1])) {
+					EntityPlayer player = players.get(i);
+					for (int j = 0; j < RegisterSkill.skillList.size(); j++) {
+						SkillLevelBase skillBase = RegisterSkill.skillList.get(j);
+						SkillLevelBase skill = (SkillLevelBase) skillBase.get(player, skillBase.skillId);
+						
+						skill.setXP(0);
+						RPGCore.network.sendTo(new LevelPacket(skill.getXP(), -1, skill.skillId), (EntityPlayerMP) player);
+					}
+					GlobalLevel glevel = (GlobalLevel) GlobalLevel.get(player);
+					EquippedSkills equippedSkills = (EquippedSkills) EquippedSkills.get(player);
+					
+					glevel.setXP(0);
+					glevel.setSkillPoints(0);
+					
+					equippedSkills.clearSkills();
+					RPGCore.network.sendTo(new EquippedSkillsPacket("", "", "", "", "", "", "", "", "", "", "", ""), (EntityPlayerMP) player);
+					RPGCore.network.sendTo(new LevelPacket((int)(glevel.getXPGlobal()*10), glevel.getSkillPoints(), glevel.skillId), (EntityPlayerMP) player);
+					player.addChatMessage(new ChatComponentText(red + "[RPGCore] Your skills have been reset by an admin."));
+					
+					sender.addChatMessage(new ChatComponentText(green + player.getDisplayName() + "'s skills have been reset."));
+					return;
+				}
+			}
+			sender.addChatMessage(new ChatComponentText(red + "Player not found."));
 		}
 	}
 }
