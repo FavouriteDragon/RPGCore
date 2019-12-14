602bea0f920bc24f30ba936a47f0129db67e61aa
FavouriteDragon <ninjadragon008@icloud.com>
2019-12-13 20:19:58 -0800

    working on globallevel right now

--- src/main/java/co/uk/silvania/rpgcore/skills/GlobalLevel.java
+++ src/main/java/co/uk/silvania/rpgcore/skills/GlobalLevel.java
@@ -1,45 +1,58 @@
 package co.uk.silvania.rpgcore.skills;
 
-import java.util.ArrayList;
-
+import co.uk.silvania.rpgcore.RPGCoreConfig;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import net.minecraft.client.Minecraft;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.nbt.NBTBase;
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.network.play.client.CPacketPlayerAbilities;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.ChatComponentText;
-import net.minecraft.util.ChatStyle;
 import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.text.Style;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
 import net.minecraftforge.common.IExtendedEntityProperties;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.capabilities.Capability;
-import net.minecraftforge.common.capabilities.ICapabilityProvider;
 import net.minecraftforge.common.capabilities.ICapabilitySerializable;
 import net.minecraftforge.event.entity.EntityEvent.EntityConstructing;
+import net.minecraftforge.fml.common.FMLCommonHandler;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GlobalLevel extends SkillLevelBase implements ICapabilitySerializable<NBTBase> {
-	
+
 	public static String staticSkillId;
-	
+
 	public String skillName;
 	public String skillId;
-	
+
 	public float xpGlobal;
 	public int skillPoints;
-	
+
 	public GlobalLevel(EntityPlayer player, String skillID) {
 		super(skillID);
 		skillId = skillID;
 		staticSkillId = skillID;
 		this.xpGlobal = 0;
 		this.skillPoints = 0;
+	}
+
+	public static final void register(EntityPlayer player) {
+		player.registerExtendedProperties(GlobalLevel.staticSkillId, new GlobalLevel(player, staticSkillId));
+	}
+
+	public static IExtendedEntityProperties get(EntityPlayer player) {
+		return player.getExtendedProperties(staticSkillId);
 	}
 
 	@Override
@@ -56,76 +69,85 @@ public class GlobalLevel extends SkillLevelBase implements ICapabilitySerializab
 		xpGlobal = nbt.getFloat(skillId + "xp");
 		skillPoints = nbt.getInteger("skillPoints");
 	}
-	
-	public static final void register(EntityPlayer player) {
-		player.registerExtendedProperties(GlobalLevel.staticSkillId, new GlobalLevel(player, staticSkillId));
+
+	@Override
+	public void init(Entity entity, World world) {
 	}
 
-	@Override public void init(Entity entity, World world) {}
-	
 	public String getXPTotalForPrint() {
 		return getXPProgressForPrint() + " (" + (int) getXPGlobal() + ")";
 	}
 
 	public String getXPProgressForPrint() {
-		return (int) (getXPGlobal() - getXpForLevel(getLevel()-1)) + "/" + (getXpForLevel(getLevel()) - getXpForLevel(getLevel()-1));
+		return (int) (getXPGlobal() - getXpForLevel(getLevel() - 1)) + "/" + (getXpForLevel(getLevel()) - getXpForLevel(getLevel() - 1));
 	}
-	
+
 	public int slotUnlockedLevel(int slot) {
 		if ((slot >= 3 && slot <= 9)) {
 			return -1;
 		}
-		
-		if (slot == 0)  { return 3; }
-		if (slot == 1)  { return 10; }
-		if (slot == 11) { return 15; }
-		if (slot == 2)  { return 25; }
-		if (slot == 10) { return 50; }
-		
+
+		if (slot == 0) {
+			return 3;
+		}
+		if (slot == 1) {
+			return 10;
+		}
+		if (slot == 11) {
+			return 15;
+		}
+		if (slot == 2) {
+			return 25;
+		}
+		if (slot == 10) {
+			return 50;
+		}
+
 		return 999;
 	}
-	
+
 	public int getLevel() {
-		int base = config.baseXp;
-		int previousXp = config.baseXp;
+		int base = RPGCoreConfig.baseXp;
+		int previousXp = RPGCoreConfig.baseXp;
 		int level = 1;
-		
+
 		while (xpGlobal >= previousXp) {
-			previousXp += base + ((base / 100.0) * ((level*5) * (35 + ((level/10)*10))));
+			previousXp += base + ((base / 100.0) * ((level * 5) * (35 + ((level / 10) * 10))));
 			level++;
 		}
 		return level;
 	}
-	
+
 	public int getXpForLevel(int level) {
 		if (level < 1) {
 			return 0;
 		}
-		int base = config.baseXp;
-		int xpForLevel = config.baseXp;
-		
+		int base = RPGCoreConfig.baseXp;
+		int xpForLevel = RPGCoreConfig.baseXp;
+
 		for (int i = 1; i < level; i++) {
-			xpForLevel += base + ((base / 100.0) * ((i*5) * (35 + ((i/10)*10))));
+			xpForLevel += base + ((base / 100.0) * ((i * 5) * (35 + ((i / 10) * 10))));
 		}
 		return xpForLevel;
 	}
-	
-	public float xpToNextLevel() {		
-		return (getXpForLevel(getLevel())) - (int)getXPGlobal();
+
+	public float xpToNextLevel() {
+		return (getXpForLevel(getLevel())) - (int) getXPGlobal();
 	}
-	
+
 	public void levelUpGlobal(EntityPlayer player, float xpAdd) {
-		ChatStyle cht = new ChatStyle();
+		Style style = new Style();
 		int lvl = getLevelFromXP(getXPGlobal() + xpAdd);
-		player.addChatMessage(new ChatComponentText("Level up! Your Global Level is now " + lvl).setChatStyle(cht.setColor(EnumChatFormatting.GOLD)));
-		player.addChatMessage(new ChatComponentText("You currently have " + (getSkillPoints()+config.skillPointsPerLevel) + " Skill Points available.").setChatStyle(cht.setColor(EnumChatFormatting.GOLD)));
-		skillPoints += config.skillPointsPerLevel;
+		player.sendMessage(new TextComponentString("Level up! Your Global Level is now " + lvl).setStyle(style.setColor(TextFormatting.GOLD).setBold(true)));
+		player.sendMessage(new TextComponentString("You currently have " + (getSkillPoints() + RPGCoreConfig.skillPointsPerLevel) + " Skill Points available.")
+				.setStyle(style.setColor(TextFormatting.GOLD)));
+		skillPoints += RPGCoreConfig.skillPointsPerLevel;
 		verbose(player.getDisplayName() + " reached Global Level " + lvl + " and now has " + getSkillPoints() + " skill points.");
-		
-		ArrayList<EntityPlayer> players = (ArrayList<EntityPlayer>) MinecraftServer.getServer().getConfigurationManager().playerEntityList;
+
+		List<EntityPlayerMP> players =  FMLCommonHandler.instance().getMinecraftServerInstance().getPlayerList().getPlayers();
 		for (int i = 0; i < players.size(); i++) {
 			if (players.get(i) != player) {
-				players.get(i).addChatMessage(new ChatComponentText(player.getDisplayName() + " is now Level " + lvl + "!").setChatStyle(cht.setColor(EnumChatFormatting.GOLD)));
+				players.get(i).sendMessage(new TextComponentString(player.getDisplayName() + " is now Level " + lvl + "!").setStyle(style.setColor(TextFormatting.GOLD)));
 			}
 		}
 	}
@@ -133,36 +155,32 @@ public class GlobalLevel extends SkillLevelBase implements ICapabilitySerializab
 	public void setXP(float xpSet) {
 		xpGlobal = xpSet;
 	}
-	
+
 	public float getXPGlobal() {
 		return xpGlobal;
 	}
-	
-	public void setSkillPoints(int points) {
-		skillPoints = points;
-	}
-	
+
 	public int getSkillPoints() {
 		return skillPoints;
 	}
-	
+
+	public void setSkillPoints(int points) {
+		skillPoints = points;
+	}
+
 	@Override
 	public void forceAddXP(float xpAdd, EntityPlayer player) {
-		if ((xpAdd+getXPGlobal()) >= getXpForLevel(getLevel())) {
+		if ((xpAdd + getXPGlobal()) >= getXpForLevel(getLevel())) {
 			levelUpGlobal(player, xpAdd);
 		}
 		xpGlobal += xpAdd;
 	}
-	
+
 	@SubscribeEvent
 	public void onEntityConstructing(EntityConstructing event) {
 		if (event.entity instanceof EntityPlayer) {
-			event.entity.registerExtendedProperties(skillId, new GlobalLevel((EntityPlayer)event.entity, skillId));
+			event.entity.registerExtendedProperties(skillId, new GlobalLevel((EntityPlayer) event.entity, skillId));
 		}
-	}
-	
-	public static IExtendedEntityProperties get(EntityPlayer player) {
-		return player.getExtendedProperties(staticSkillId);
 	}
 
 	@Override
@@ -175,10 +193,18 @@ public class GlobalLevel extends SkillLevelBase implements ICapabilitySerializab
 		return "Global Level";
 	}
 
-	@Override public void openGui() {}
-	@Override public void addDescription() {}
-	@Override public void activateSkill(EntityPlayer player, World world) {}
-	
+	@Override
+	public void openGui() {
+	}
+
+	@Override
+	public void addDescription() {
+	}
+
+	@Override
+	public void activateSkill(EntityPlayer player, World world) {
+	}
+
 	@Override
 	public ResourceLocation skillIcon() {
 		return null;
